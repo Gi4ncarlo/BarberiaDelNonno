d61f709f0af0e494ccc1924ee054e38face49146
Giancarlo <giancarlozanarini9@gmail.com>
2024-12-16 14:53:34 -0300

    Deploy v1.4 Barberia

--- back/src/config/data-source.ts
+++ back/src/config/data-source.ts
@@ -5,15 +5,11 @@ import { Appointment } from "../entities/Appointment";
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "dpg-ctg61kl6l47c73d898a0-a",
-    port: 5432,
-    username: "barberia_4n2h_user",
-    password: "5Vu8XYflNiihPCfqnBfHfvC68YqUhorF",
-    database: "barberia_4n2h",
-   // dropSchema: true,
+    url: "postgresql://barberia_4n2h_user:5Vu8XYflNiihPCfqnBfHfvC68YqUhorF@dpg-ctg61kl6l47c73d898a0-a.oregon-postgres.render.com/barberia_4n2h",  // URL completa de Render
     synchronize: true,
     logging: false,
     entities: [User, Credentials, Appointment],
     subscribers: [],
     migrations: [],
-})
+    ssl: { rejectUnauthorized: false },
+});
